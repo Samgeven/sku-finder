704c801bc2cc749bdcb986f19c43f9c9e732412b
Samgeven <sam.geven@mail.ru>
2020-10-02 01:16:29 +0300

    deploy.sh hotfix

--- deploy.sh
+++ deploy.sh
@@ -16,6 +16,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:Samgeven/sku-finder.git master:gh-pages
 
 cd -
